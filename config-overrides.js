7f2d92159b08cbafc063112d7d189d223c248c53
keeping <starunaway@gmail.com>
2020-06-06 14:10:22 +0800

    feat: 🎸 copy

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const {override, fixBabelImports, addLessLoader} = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('antd', {
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      '@primary-color': '#1da57a',
+    },
+  })
+);
