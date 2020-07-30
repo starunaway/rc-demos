7a012c8cf06ee988f7fcfecb2a74aff59a4aafc1
keeping <starunaway@gmail.com>
2020-07-30 21:45:59 +0800

    feat: 🎸 use function components

--- src/pages/exportExcel/index.js
+++ src/pages/exportExcel/index.js
@@ -3,21 +3,19 @@ import {Table, Button} from 'antd';
 import data, {exportData} from './data';
 import columns from './columns';
 import ExcelExport from './utils';
-class ExportExcel extends Component {
-  handleExport = () => {
+
+export default () => {
+  const handleExport = () => {
     let excel = new ExcelExport();
     excel.setFileName('测试').addSheet('sheet1', exportData, columns).export();
   };
-  render() {
-    return (
-      <div>
-        <div className='page-top-btns'>
-          <Button onClick={this.handleExport}>导出excel</Button>
-        </div>
-        <Table columns={columns} dataSource={data} bordered />
-      </div>
-    );
-  }
-}
 
-export default ExportExcel;
+  return (
+    <div>
+      <div className='page-top-btns'>
+        <Button onClick={handleExport}>导出excel</Button>
+      </div>
+      <Table columns={columns} dataSource={data} bordered />
+    </div>
+  );
+};
