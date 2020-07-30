2301287ca121965d1bc72edb7459f951ab0445d5
keeping <starunaway@gmail.com>
2020-07-30 21:57:09 +0800

    feat: 🎸 use func components

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
 import './index.less';
 
-class Home extends Component {
-  render() {
-    return <div className='home'>react snippets</div>;
-  }
-}
-
-export default Home;
+export default () => {
+  return <div className='home'>react snippets</div>;
+};
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,27 +5,21 @@ import {Redirect, Switch, Route} from 'react-router-dom';
 import ROUTES from '@constants/routes';
 const {Sider, Content} = Layout;
 
-class Main extends Component {
-  render() {
-    return (
-      <Layout style={{height: '100%'}}>
-        <Sider>
-          <LeftNav></LeftNav>
-        </Sider>
-        <Layout>
-          <Content>
-            <Switch>
-              {ROUTES.map((route) => (
-                <Route path={route.path} component={route.component} key={route.path} />
-              ))}
+export default () => (
+  <Layout style={{height: '100%'}}>
+    <Sider>
+      <LeftNav></LeftNav>
+    </Sider>
+    <Layout>
+      <Content>
+        <Switch>
+          {ROUTES.map((route) => (
+            <Route path={route.path} component={route.component} key={route.path} />
+          ))}
 
-              <Redirect to='/home'></Redirect>
-            </Switch>
-          </Content>
-        </Layout>
-      </Layout>
-    );
-  }
-}
-
-export default Main;
+          <Redirect to='/home'></Redirect>
+        </Switch>
+      </Content>
+    </Layout>
+  </Layout>
+);
